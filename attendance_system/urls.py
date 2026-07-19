b999b8ac8d0b6e014673f4c86f25df9d61d90b8b
Gaurav Chaudhary <gauravch410@gmail.com>
2026-07-19 12:18:18 +0530

    Update urls.py

--- attendance_system/urls.py
+++ attendance_system/urls.py
@@ -1,16 +1,45 @@
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import include, path
 
 from attendance.views.auth_views import (
     CustomLoginView,
     CustomLogoutView,
+    CustomPasswordResetCompleteView,
+    CustomPasswordResetConfirmView,
+    CustomPasswordResetDoneView,
+    CustomPasswordResetView,
 )
 
+
+def home_redirect(request):
+    return redirect("login")
+
+
 urlpatterns = [
+
+    # ==========================================================
+    # Home
+    # ==========================================================
+
+    path(
+        "",
+        home_redirect,
+        name="home",
+    ),
+
+    # ==========================================================
+    # Django Admin
+    # ==========================================================
+
     path(
         "admin/",
         admin.site.urls,
     ),
+
+    # ==========================================================
+    # Authentication
+    # ==========================================================
 
     path(
         "login/",
@@ -23,6 +52,38 @@ urlpatterns = [
         CustomLogoutView.as_view(),
         name="logout",
     ),
+
+    # ==========================================================
+    # Password Reset
+    # ==========================================================
+
+    path(
+        "password-reset/",
+        CustomPasswordResetView.as_view(),
+        name="password_reset",
+    ),
+
+    path(
+        "password-reset/done/",
+        CustomPasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+
+    path(
+        "password-reset-confirm/<uidb64>/<token>/",
+        CustomPasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+
+    path(
+        "password-reset-complete/",
+        CustomPasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
+
+    # ==========================================================
+    # Attendance Application
+    # ==========================================================
 
     path(
         "",
